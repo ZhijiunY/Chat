2278475a8c656e72c76749b5abc3a43857d0fd10
ZhijiunY <yzj90596@gmail.com>
2023-11-29 15:28:56 +0800

    updateFile

---
+++ server/main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+// 處理和客戶端的通訊
+func process(conn net.Conn) {
+	// 這裡需要延時關閉conn
+	defer conn.Close()
+
+}
+
+func main() {
+	fmt.Println("服務器在8889端口監聽...")
+	listen, err := net.Listen("tcp", "0.0.0.0:8889")
+	defer listen.Close()
+
+	if err != nil {
+		fmt.Println("net.Listen err=", err)
+		return
+	}
+
+	for {
+		fmt.Println("等待客戶端來連接服務器......")
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("listen.Accept err=", err)
+		}
+
+		go process(conn)
+	}
+
+}
